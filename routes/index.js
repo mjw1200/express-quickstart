234cf6ee2d0c0f4f7e6e1b25e7deb54504939d08
Mike Waldron <mjw1200@yahoo.com>
2023-06-25 19:12:33 -0600

    Add axios, and a sample call

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,41 @@ router.get("/", function (req, res, next) {
 
 router.post("/authenticated", function (req, res, next) {
     console.log(`BLAM! ${JSON.stringify(req.body)}`);
-    res.render("authenticated", { title: "Now You're Authenticated", password: req.body.password, username: req.body.username });
+
+    axiosRequest().then(data => {
+        console.log(data);
+        res.render("authenticated", { title: "Now You're Authenticated", password: req.body.password, username: req.body.username });
+    }).catch(error => {
+        res.status(400).send("<!DOCTYPE html><html><head><title>There Was Trouble</title><link rel=\"stylesheet\" href=\"/stylesheets/style.css\"></head><body><h1>Ruh-roh, Raggy</h1><p>Aroo?</p></body></html>")
+    });
 });
+
+async function axiosRequest() {
+    return new Promise((resolve, reject) => {
+        const axios = require('axios');
+        let data = JSON.stringify({
+            "cat": "meow",
+            "dog": "woof"
+        });
+
+        let config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: 'http://ruthisaschmoo.net:3000',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: data
+        };
+
+        axios.request(config)
+            .then((response) => {
+                resolve(response.data);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
 
 module.exports = router;
